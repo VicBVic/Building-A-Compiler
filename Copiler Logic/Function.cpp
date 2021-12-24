1d02a964a5ca822d5e16a67d1a4e20077b57e15d
RobBobBot <92254454+RobBobBot@users.noreply.github.com>
2021-12-24 21:45:14 +0200

    implementare functii cu trie

---
+++ Copiler Logic/Function.cpp
@@ -0,0 +1,7 @@
+#include "Function.h"
+
+Function::Function(Trie &global) {
+	this->global = &global;
+	//se initializeaza params
+}
+
---
+++ Copiler Logic/Function.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+#include "Trie.h"
+#include "Argument.h"
+class Function
+{
+private:
+	std::string params[(int)1e5];
+	Trie *local;
+	Trie *global;
+	Function(Trie &global);
+	Argument *start;
+public:
+	bool chageParams();
+	void Start();
+};
