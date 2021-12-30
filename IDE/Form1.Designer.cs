186810f8e3cf652034450acd4600984ba76c621d
RobBobBot <92254454+RobBobBot@users.noreply.github.com>
2021-12-30 13:29:59 +0200

    Refacere layout

--- IDE/Form1.Designer.cs
+++ IDE/Form1.Designer.cs
@@ -30,20 +30,98 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.ProjectList = new System.Windows.Forms.FlowLayoutPanel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.TextBox = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.ProjectList = new System.Windows.Forms.FlowLayoutPanel();
+            this.run = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.TextBox)).BeginInit();
+            this.ProjectList.SuspendLayout();
             this.SuspendLayout();
             // 
-            // ProjectList
+            // splitContainer1
             // 
-            this.ProjectList.Dock = System.Windows.Forms.DockStyle.Left;
-            this.ProjectList.Location = new System.Drawing.Point(0, 0);
-            this.ProjectList.Margin = new System.Windows.Forms.Padding(5);
-            this.ProjectList.Name = "ProjectList";
-            this.ProjectList.Size = new System.Drawing.Size(150, 450);
-            this.ProjectList.TabIndex = 0;
-            this.ProjectList.Paint += new System.Windows.Forms.PaintEventHandler(this.ProjectList_Paint);
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.flowLayoutPanel2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 25;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(800, 25);
+            this.flowLayoutPanel2.TabIndex = 1;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.flowLayoutPanel1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.splitContainer3);
+            this.splitContainer2.Size = new System.Drawing.Size(800, 421);
+            this.splitContainer2.SplitterDistance = 100;
+            this.splitContainer2.TabIndex = 2;
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            this.splitContainer3.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.ProjectList);
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add(this.TextBox);
+            this.splitContainer3.Size = new System.Drawing.Size(696, 421);
+            this.splitContainer3.SplitterDistance = 26;
+            this.splitContainer3.TabIndex = 1;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(100, 421);
+            this.flowLayoutPanel1.TabIndex = 3;
             // 
             // TextBox
             // 
@@ -64,40 +142,75 @@
             this.TextBox.CharWidth = 10;
             this.TextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.TextBox.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
-            this.TextBox.Dock = System.Windows.Forms.DockStyle.Right;
-            this.TextBox.Font = new System.Drawing.Font("Courier New", 9.75F);
+            this.TextBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.TextBox.ForeColor = System.Drawing.Color.Coral;
             this.TextBox.IsReplaceMode = false;
-            this.TextBox.Location = new System.Drawing.Point(160, 0);
+            this.TextBox.Location = new System.Drawing.Point(0, 0);
             this.TextBox.Margin = new System.Windows.Forms.Padding(5);
             this.TextBox.Name = "TextBox";
             this.TextBox.Paddings = new System.Windows.Forms.Padding(0);
             this.TextBox.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
             this.TextBox.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("TextBox.ServiceColors")));
-            this.TextBox.Size = new System.Drawing.Size(640, 450);
-            this.TextBox.TabIndex = 1;
+            this.TextBox.Size = new System.Drawing.Size(696, 391);
+            this.TextBox.TabIndex = 2;
             this.TextBox.Text = "fastColoredTextBox1";
             this.TextBox.Zoom = 100;
-            this.TextBox.Load += new System.EventHandler(this.TextBox_Load);
+            // 
+            // ProjectList
+            // 
+            this.ProjectList.Controls.Add(this.run);
+            this.ProjectList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ProjectList.Location = new System.Drawing.Point(0, 0);
+            this.ProjectList.Margin = new System.Windows.Forms.Padding(5);
+            this.ProjectList.Name = "ProjectList";
+            this.ProjectList.Size = new System.Drawing.Size(696, 26);
+            this.ProjectList.TabIndex = 1;
+            // 
+            // run
+            // 
+            this.run.Location = new System.Drawing.Point(3, 3);
+            this.run.Name = "run";
+            this.run.Size = new System.Drawing.Size(50, 25);
+            this.run.TabIndex = 0;
+            this.run.Text = "Run";
+            this.run.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.TextBox);
-            this.Controls.Add(this.ProjectList);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.TextBox)).EndInit();
+            this.ProjectList.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.SplitContainer splitContainer3;
         private System.Windows.Forms.FlowLayoutPanel ProjectList;
+        private System.Windows.Forms.Button run;
         private FastColoredTextBoxNS.FastColoredTextBox TextBox;
     }
 }
